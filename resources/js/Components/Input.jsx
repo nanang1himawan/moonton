276162379457d5d76e928e4444e74a4b955ce088
nanang1himawan <44635365+nanang1himawan@users.noreply.github.com>
2022-08-31 00:32:25 +0700

    feat: slicing sign in and sign up

--- resources/js/Components/Input.jsx
+++ resources/js/Components/Input.jsx
@@ -1,14 +1,34 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
+import Proptypes from "prop-types";
+
+Input.prototype = {
+    type: Proptypes.oneOf(["text", "email", "password", "number", "file"]),
+    name: Proptypes.string,
+    value: Proptypes.oneOf([Proptypes.string, Proptypes.number]),
+    defaultValue: Proptypes.oneOf([Proptypes.string, Proptypes.number]),
+    className: Proptypes.string,
+    variant: Proptypes.oneOf(["primary", "error", "primary-outline"]),
+    autoComplete: Proptypes.string,
+    require: Proptypes.bool,
+    isFocused: Proptypes.bool,
+    handleChange: Proptypes.func,
+    placeholder: Proptypes.string,
+    isError: Proptypes.bool,
+};
 
 export default function Input({
-    type = 'text',
+    type = "text",
     name,
     value,
+    defaultValue,
     className,
+    variant = "primary",
     autoComplete,
     required,
     isFocused,
     handleChange,
+    placeholder,
+    isError,
 }) {
     const input = useRef();
 
@@ -24,15 +44,18 @@ export default function Input({
                 type={type}
                 name={name}
                 value={value}
-                className={
-                    `border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm ` +
-                    className
-                }
+                defaultValue={defaultValue}
+                className={`rounded-2xl bg-form-bg py-[13px] px-7 w-full ${
+                    isError && "input-error"
+                } input-${variant} ${className}`}
                 ref={input}
                 autoComplete={autoComplete}
                 required={required}
                 onChange={(e) => handleChange(e)}
+                placeholder={placeholder}
             />
         </div>
     );
 }
+
+
--- routes/web.php
+++ routes/web.php
@@ -32,8 +32,19 @@ Route::get('/', function () {
     ]);
 });
 
+Route::redirect('/','/prototype/login');
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::prefix('prototype')->name('prototype.')->group(function(){
+    route::get('/login', function(){
+        return Inertia::render('Prototype/Login');
+    })->name('login');
+    route::get('/register', function(){
+        return Inertia::render('Prototype/Register');
+    })->name('register');
+
+});
 require __DIR__.'/auth.php';
