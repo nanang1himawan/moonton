de918fe287806c8c8231500298690e6e16ae6156
nanang1himawan <44635365+nanang1himawan@users.noreply.github.com>
2022-08-31 22:28:05 +0700

    feat: tambahan

--- resources/js/Components/MovieCard.jsx
+++ resources/js/Components/MovieCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from "@inertiajs/inertia-react";
 import Proptype from "prop-types";
 
 MovieCard.Proptype = {
@@ -5,7 +6,7 @@ MovieCard.Proptype = {
     name: Proptype.string.isRequired,
     category: Proptype.string.isRequired,
     thumbnail: Proptype.string.isRequired,
-}
+};
 
 export default function MovieCard({ slug, name, category, thumbnail }) {
     return (
@@ -29,7 +30,10 @@ export default function MovieCard({ slug, name, category, thumbnail }) {
             >
                 <img src="/icons/ic_play.svg" className="" width="50" alt="" />
             </div>
-            <a href={slug} className="inset-0 absolute z-50"></a>
+            <Link
+                href={route("prototype.movie.show", slug)}
+                className="inset-0 absolute z-50"
+            ></Link>
         </div>
     );
 }
--- resources/js/Pages/Prototype/Movie/Show.jsx
+++ resources/js/Pages/Prototype/Movie/Show.jsx
@@ -1,3 +1,46 @@
-export default function Show(){
-    return "Show Page";
+export default function Show() {
+    return (
+        <section
+            class="mx-auto w-screen relative watching-page font-poppins"
+            id="stream"
+        >
+            <video-js
+                id="stream-video"
+                class="overflow-hidden h-screen w-screen"
+            >
+                <source
+                    src="https://d33kv075lir7n3.cloudfront.net/Details+Screen+Part+Final.mp4"
+                    type="video/mp4"
+                />
+                <p class="vjs-no-js text-twmdark">
+                    To view this video please enable JavaScript, and consider
+                    upgrading to a web browser that
+                    <a
+                        href="https://videojs.com/html5-video-support/"
+                        target="_blank"
+                    >
+                        supports HTML5 video
+                    </a>
+                </p>
+            </video-js>
+
+            {/* <!-- Button back to dashboard --> */}
+            <div class="absolute top-5 left-5 z-20">
+                <a href="/">
+                    <img
+                        src="assets/icons/ic_arrow-left.svg"
+                        class="transition-all btn-back w-[46px]"
+                        alt="stream"
+                    />
+                </a>
+            </div>
+
+            {/* <!-- Video Title --> */}
+            <div class="absolute title-video top-7 left-1/2 -translate-x-1/2 max-w-[310px] md:max-w-[620px] text-center">
+                <span class="font-medium text-2xl transition-all text-white drop-shadow-md select-none">
+                    Details Screen Part Final
+                </span>
+            </div>
+        </section>
+    );
 }
