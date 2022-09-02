af7851686ef8d44a063cbf1e7252dbd0bb0141d9
nanang1himawan <44635365+nanang1himawan@users.noreply.github.com>
2022-09-02 09:48:16 +0700

    feat: delete restore

--- app/Http/Controllers/Admin/MovieController.php
+++ app/Http/Controllers/Admin/MovieController.php
@@ -112,6 +112,18 @@ class MovieController extends Controller
      */
     public function destroy(Movie $movie)
     {
-        //
+        $movie->delete();
+        return redirect(route('admin.dashboard.movie.index'))->with([
+            'message' => "Movie deleted successfully",
+            'type' => 'success'
+        ]);
+    }
+    public function restore($movie)
+    {
+        Movie::withTrashed()->find($movie)->restore();
+        return redirect(route('admin.dashboard.movie.index'))->with([
+            'message' => "Movie restored successfully",
+            'type' => 'success'
+        ]);
     }
 }
